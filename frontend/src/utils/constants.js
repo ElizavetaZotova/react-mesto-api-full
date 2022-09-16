8378959aa58a04fd8c652c7ee1c99b23ec6ee971
Elizaveta Zotova <e.zotova@dodopizza.com>
2022-09-16 14:53:15 +0500

    feat: обновлен роут

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://liza.nomoredomains.sbs/api';
+export const baseUrl = 'https://api.liza.nomoredomains.sbs/api';
 export const authorizationToken = '5a241458-7ba3-4de4-b2d2-8b278af081f0';
 
-export const authBaseUrl = 'https://liza.nomoredomains.sbs/api';
+export const authBaseUrl = 'https://api.liza.nomoredomains.sbs/api';
