09286e64067172f5865156cfe5719ed70af88c7b
Elizaveta Zotova <e.zotova@dodopizza.com>
2022-09-16 14:48:40 +0500

    feat: изменены корневые роуты

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://localhost:3000';
+export const baseUrl = 'api';
 export const authorizationToken = '5a241458-7ba3-4de4-b2d2-8b278af081f0';
 
-export const authBaseUrl = 'http://localhost:3000';
+export const authBaseUrl = 'api';
