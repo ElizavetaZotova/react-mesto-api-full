eb405715da5046978d6362791b827932fe78d3f6
Elizaveta Zotova <e.zotova@dodopizza.com>
2022-09-18 20:29:24 +0500

    feat: поправлено удаление лайка

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -77,7 +77,7 @@ function App() {
   }
 
   function handleCardLike(card) {
-    const isLiked = card.likes.some(i => i._id === currentUser._id);
+    const isLiked = card.likes.some(userId => userId === currentUser._id);
 
     api.changeLikeCardStatus(card._id, !isLiked)
       .then((res) => {
